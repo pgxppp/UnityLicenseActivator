e8f3208b950aa54fdf50d25123820799c1100ab8
Rapilias <rapilias.rest@gmail.com>
2022-08-02 16:51:15 +0900

    Update filepath / I/F

--- UnityLicenseActivator/UnityLicenseActivator/src/UnityLicenseCommand.cs
+++ UnityLicenseActivator/UnityLicenseActivator/src/UnityLicenseCommand.cs
@@ -13,7 +13,8 @@ namespace UnityLicenseActivator
 {
     public class UnityLicenseCommand : ConsoleAppBase
     {
-        private ChromiumDriver driver = null;
+        private static readonly string UlfPath = Path.Combine(Directory.GetCurrentDirectory(), "Ulf");
+        private WebDriver driver = null;
         private WebDriverWait waiter = null;
 
         [Command("auth-ulf")]
@@ -25,6 +26,10 @@ namespace UnityLicenseActivator
         {
             var fullPath = Path.GetFullPath(alfFilePath);
             Console.WriteLine($"Alf: {alfFilePath} -> {fullPath}");
+
+            if (Directory.Exists(UlfPath))
+                Directory.Delete(UlfPath, recursive: true);
+            Directory.CreateDirectory(UlfPath);
 
             this.driver = CreateDriver();
             this.waiter = new WebDriverWait(new SystemClock(), this.driver, timeout: TimeSpan.FromSeconds(15.0),  sleepInterval: TimeSpan.FromSeconds(0.1));
@@ -99,13 +104,11 @@ namespace UnityLicenseActivator
             string ulfFilePath = string.Empty;
             await Spinner.StartAsync($"Download Ulf...", async (spinner) =>
             {
-                var previousFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.ulf");
                 this.waiter.Until(m => m.FindElement(By.Name("commit")).Displayed);
                 this.driver.FindElement(By.Name("commit")).Click();
-                this.waiter.Until(m => previousFiles.Length != Directory.GetFiles(Directory.GetCurrentDirectory(), "*.ulf").Length);
+                this.waiter.Until(m => Directory.GetFiles(UlfPath).Length > 0);
 
-                var activeFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.ulf");
-                ulfFilePath = activeFiles.Concat(previousFiles).GroupBy(m => m).Where(m => m.Count() == 1).First().First();
+                ulfFilePath = Directory.GetFiles(Directory.GetCurrentDirectory()).First();
 
                 // wait file downloaded
                 await Task.Delay(1000);
@@ -121,7 +124,7 @@ namespace UnityLicenseActivator
 
             // ファイルダウンロード時の保存先確認ウインドウを抑制する
             options.AddUserProfilePreference("disable-popup-blocking", "true");
-            options.AddUserProfilePreference("download.default_directory", Directory.GetCurrentDirectory());
+            options.AddUserProfilePreference("download.default_directory", UlfPath);
             //ブラウザ非表示
             // service.HideCommandPromptWindow = true;
 
